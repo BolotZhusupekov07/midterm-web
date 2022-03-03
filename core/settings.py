378c7b1af04ac85c7c512769aa4c6f5c94a59ae8
Bolot Zhusupekov <73470679+BolotZhusupekov07@users.noreply.github.com>
2022-03-03 19:13:51 +0600

    feat: Dockerized project

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = int(os.environ.get("DEBUG"))
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split()
 
