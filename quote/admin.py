0ac2e0e87dbe78992f625493f378dd8153fbf30f
Bolot Zhusupekov <73470679+BolotZhusupekov07@users.noreply.github.com>
2022-03-03 15:44:41 +0600

    feat: Quote model admin configurations

--- quote/admin.py
+++ quote/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import Quote
 
-admin.site.register(Quote)
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'quote',
+        'no_of_letters',
+        'no_of_vowels',
+        'no_of_consonants',
+        'avg_len_of_words',
+        'three_longest_words'
+    ]
+    ordering = ['-id']
