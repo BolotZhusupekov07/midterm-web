62a5ceb3aec746ad240bab54d18228ea69c676fb
Bolot Zhusupekov <73470679+BolotZhusupekov07@users.noreply.github.com>
2022-03-05 22:25:23 +0600

    fix: add crsf_trusted_origins configuration to settings

--- core/settings.py
+++ core/settings.py
@@ -91,6 +91,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+CSRF_TRUSTED_ORIGINS=['https://*.herokuapp.com']
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
